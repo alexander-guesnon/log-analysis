ff6069b1ee90b869c7db09cfc358f53f382ac2d1
alexgooz <alexgooz92fs@gmail.com>
2017-09-02 15:19:22 -0700

    Making an outline of the entire program.

--- db-analysis-tool.py
+++ db-analysis-tool.py
@@ -1,1 +1,21 @@
-print ("hello world")
+#1 import libraries to query PSQL
+
+'''
+Input: void
+Output: void
+Function: This function will report the current top 3 articles, the current
+top authors, and errors that are over 1% of all requests.
+'''
+def DB_Status():
+    #2 run a query to find the 3 top articles of all time
+    #3 store in variable (top_3_articals)
+    #4 run a query to find the top authors of all time.
+    #5 store in variable (top_autohors)
+    #6 run a query to find what days resulted in having their total requests error over 1%
+    #7 store in variable (important_errors)
+    #8 print out information in a readable format for the user
+    print ("hello world")
+
+
+if __name__ == "__main__":
+    DB_Status()
