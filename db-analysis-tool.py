64c239383b0944b21b144d69452d04882485e7db
alexgooz <alexgooz92fs@gmail.com>
2017-09-03 15:03:49 -0700

    did arithmetic on the data to show errors

--- db-analysis-tool.py
+++ db-analysis-tool.py
@@ -80,6 +80,116 @@ SELECT time, status FROM log LIMIT 1000;
 https://www.postgresql.org/docs/9.2/static/functions-datetime.html
 date_trunc(text, timestamp)	timestamp	Truncate to specified precision; see also Section 9.9.2	date_trunc('hour', timestamp '2001-02-16 20:38:40')	2001-02-16 20:00:00
 
+SELECT date_trunc('day',time) FROM log LIMIT 1000;
+
+       date_trunc
+------------------------
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+ 2016-07-01 00:00:00+00
+
+SELECT COUNT(*),status AS stuff FROM log GROUP BY status;
+
+  count  |     stuff
+---------+---------------
+   12908 | 404 NOT FOUND
+ 1664827 | 200 OK
+(2 rows)
+
+
+
+SELECT date_trunc('day',time) AS date, COUNT(status)
+FROM log
+WHERE status='404 NOT FOUND'
+GROUP BY date
+
+          date          | count
+------------------------+-------
+ 2016-07-28 00:00:00+00 |   393
+ 2016-07-22 00:00:00+00 |   406
+ 2016-07-26 00:00:00+00 |   396
+ 2016-07-29 00:00:00+00 |   382
+ 2016-07-06 00:00:00+00 |   420
+ 2016-07-02 00:00:00+00 |   389
+ 2016-07-17 00:00:00+00 |  1265
+ 2016-07-12 00:00:00+00 |   373
+ 2016-07-07 00:00:00+00 |   360
+ 2016-07-04 00:00:00+00 |   380
+
+
+SELECT date_trunc('day',time) AS date, COUNT(status)
+FROM log
+WHERE status='200 OK'
+GROUP BY date
+
+          date          | count
+------------------------+-------
+ 2016-07-01 00:00:00+00 | 38431
+ 2016-07-02 00:00:00+00 | 54811
+ 2016-07-03 00:00:00+00 | 54465
+ 2016-07-04 00:00:00+00 | 54523
+ 2016-07-05 00:00:00+00 | 54162
+ 2016-07-06 00:00:00+00 | 54354
+ 2016-07-07 00:00:00+00 | 54380
+ 2016-07-08 00:00:00+00 | 54666
+ 2016-07-09 00:00:00+00 | 54826
+ 2016-07-10 00:00:00+00 | 54118
+
+https://www.postgresql.org/docs/9.3/static/functions-math.html
+https://www.postgresql.org/docs/9.2/static/sql-createcast.html
+
+SELECT cast(393 as float8) /cast (38431 as float8) AS gekk ;
+
+SELECT round((cast(393 as numeric) /cast (38431 as numeric)), 3) AS
+gekk;
+
+ gekk
+-------
+ 0.010
+(1 row)
+
+
+SELECT success.date ,round(
+(cast(errors.count as numeric) /cast (success.count as numeric)), 3
+) AS failure_percentage FROM (
+    SELECT date_trunc('day',time) AS date, COUNT(status)
+    FROM log
+    WHERE status='404 NOT FOUND'
+    GROUP BY date
+    ORDER BY date
+) AS errors, (
+    SELECT date_trunc('day',time) AS date, COUNT(status)
+    FROM log
+    WHERE status='200 OK'
+    GROUP BY date
+    ORDER BY date
+) AS success;
+
+          date          | failure_percentage
+------------------------+--------------------
+ 2016-07-01 00:00:00+00 |              0.007
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.011
+ 2016-07-01 00:00:00+00 |              0.011
+ 2016-07-01 00:00:00+00 |              0.009
+ 2016-07-01 00:00:00+00 |              0.011
+ 2016-07-01 00:00:00+00 |              0.011
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.010
+ 2016-07-01 00:00:00+00 |              0.011
+ 2016-07-01 00:00:00+00 |              0.010
 
 
 '''
