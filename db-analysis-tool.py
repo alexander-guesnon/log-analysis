d15b0ebcac96361cfe3ef0de42355a6d6dc78b0e
alexgooz <alexgooz92fs@gmail.com>
2017-09-04 18:48:52 -0700

    important error table is done.

--- db-analysis-tool.py
+++ db-analysis-tool.py
@@ -1,4 +1,3 @@
-# 1 import libraries to query PSQL
 import psycopg2
 
 '''
@@ -8,116 +7,101 @@ Function: This function will report the current top 3 articles, the current
 top authors, and errors that are over 1% of all requests.
 '''
 
-'''
-              title               | views
-----------------------------------+--------
- Candidate is jerk, alleges rival | 338647
- Bears love berries, alleges bear | 253801
- Bad things gone, say good people | 170098
-(3 rows)
 
-    [('Candidate is jerk, alleges rival', 338647L), ('Bears love berries, alleges bear', 253801L), ('Bad things gone, say good people', 170098L)]
-
-
-          name          | views
-------------------------+--------
- Ursula La Multa        | 507594
- Rudolf von Treppenwitz | 423457
- Anonymous Contributor  | 170098
- Markoff Chaney         |  84557
-
-
-
-[('Ursula La Multa', Decimal('507594')), ('Rudolf von Treppenwitz', Decimal('423457')), ('Anonymous Contributor', Decimal('170098')), ('Markoff Chaney', Deci
-mal('84557'))]
-
-
-
+def print_results(t3a, ta, ie):
+    top_3_table = '''
+---------------------------------------------
+|               Top 3 Articles              |
+|-------------------------------------------|
+|             Articles             | Views  |
+|----------------------------------+--------|
 '''
 
+    t3t_template = "| {:32} | {:6} |"
 
-def print_results(ta,t3a):
-    print("test")
-    #print top 3 tables
-    #top_3_table= '''
-#---------------------------------------------
-#|               Top 3 Articles              |
-#|-------------------------------------------|
-#|             Articles             | Views  |
-#|----------------------------------+--------|
-#'''
-    #t3t_template="| {:32} | {:6} |"
-    #print (top_3_table),
-    #for x in range(len(t3a)):
-    #    print(t3t_template.format(t3a[x][0], t3a[x][1]))
-    #print("---------------------------------------------")
+    print (top_3_table),
+    for x in range(len(t3a)):
+        print(t3t_template.format(t3a[x][0], t3a[x][1]))
+        print("---------------------------------------------")
 
-#    top_authors_table= '''
-#------------------------------------
-#|            Top Authors           |
-#|----------------------------------|
-#|         Authors         | Views  |
-#|-------------------------+--------|
-#'''
-#    ta_template="| {:23} | {:6} |"
-#    print(top_authors_table),
-#    for x in range(len(ta)):
-#        print(ta_template.format(ta[x][0],ta[x][1]))
-#    print("------------------------------------")
-    #print important errors
+    top_authors_table= '''
+------------------------------------
+|            Top Authors           |
+|----------------------------------|
+|         Authors         | Views  |
+|-------------------------+--------|
+'''
+    ta_template="| {:23} | {:6} |"
+    print(top_authors_table),
+    for x in range(len(ta)):
+        print(ta_template.format(ta[x][0],ta[x][1]))
+        print("------------------------------------")
+
+    important_errors_table = '''
+----------------------------------------------------
+|                 Important Errors                 |
+|--------------------------------------------------|
+|             date          |  failure_percentage  |
+|---------------------------+----------------------|
+'''
+    ie_template = "| {:25} | {:19}% |"
+    print(important_errors_table),
+
+    for x in range(len(ie)):
+
+        print(ie_template.format( str(ie[x][0]),ie[x][1]))
+    print("----------------------------------------------------")
 
 def DB_Status():
-
     try:
-        conn = psycopg2.connect(
+        conn=psycopg2.connect(
             "dbname='news' user='vagrant' host='localhost' password='password'")
-        cur = conn.cursor()
+        cur=conn.cursor()
     except:
         print "I am unable to connect to the database"
 
-    # 2 run a query to find the 3 top articles of all time
-    #try:
-    #    cur.execute('''
-    #                SELECT articles.title,
-    #               Count(*) AS views
-    #        FROM   articles,
-    #               (SELECT Substring(path, 10) AS path
-    #                FROM   log) AS modifiedLog
-    #        WHERE  path != ''
-    #               AND modifiedLog.path = articles.slug
-    #        GROUP  BY articles.title
-    #        ORDER  BY views DESC
-    #        LIMIT  3;
-    #    ''')
-    #except:
-    #    print("Their was an error trying to querry for the top 3 articles" )
+    try:
+        cur.execute('''
+                    SELECT articles.title,
+                   Count(*) AS views
+            FROM   articles,
+                   (SELECT Substring(path, 10) AS path
+                    FROM   log) AS modifiedLog
+            WHERE  path != ''
+                   AND modifiedLog.path = articles.slug
+            GROUP  BY articles.title
+            ORDER  BY views DESC
+            LIMIT  3;
+        ''')
+    except:
+        print("Their was an error trying to querry for the top 3 articles" )
 
-    # 3 store in variable (top_3_articals)
-    #top_3_articals = cur.fetchall()
-    # 4 run a query to find the top authors of all time.
 
-    #try:
-    #    cur.execute(
-    #    '''
-    #        SELECT authors.name ,SUM(views) AS views
-    #        FROM authors, (
-    #            SELECT articles.author, articles.title ,COUNT(*) AS views
-    #            FROM articles, (
-    #                SELECT SUBSTRING(path,10) AS path
-    #                FROM log
-    #            ) AS modifiedLog
-    #            WHERE path!='' AND modifiedLog.path=articles.slug
-    #            GROUP BY articles.title, articles.author) AS articleviews
-    #        WHERE articleviews.author=authors.id
-    #        GROUP BY authors.name
-    #        ORDER BY views DESC;
-    #    '''
-    #        )
-    #except:
-    #    print("top 3 articles failed to exicute")
-    # 5 store in variable (top_autohors)
-    #top_autohors=cur.fetchall()
-    # 6 run a query to find what days resulted in having their total requests error over 1%
+    top_3_articals = cur.fetchall()
+
+    try:
+        cur.execute(
+        '''
+            SELECT authors.name ,SUM(views) AS views
+            FROM authors, (
+                SELECT articles.author, articles.title ,COUNT(*) AS views
+                FROM articles, (
+                    SELECT SUBSTRING(path,10) AS path
+                    FROM log
+                ) AS modifiedLog
+                WHERE path!='' AND modifiedLog.path=articles.slug
+                GROUP BY articles.title, articles.author) AS articleviews
+            WHERE articleviews.author=authors.id
+            GROUP BY authors.name
+            ORDER BY views DESC;
+        '''
+            )
+    except:
+        print("top 3 articles failed to exicute")
+
+    top_autohors=cur.fetchall()
+
+    cur.execute(
     '''
     SELECT final.date,
            failure_percentage
@@ -139,12 +123,12 @@ def DB_Status():
                     ORDER  BY date) AS success
             WHERE  errors.date = success.date) AS final
     WHERE  failure_percentage > 1;
-    '''
-    # 7 store in variable (important_errors)
-    # 8 print out information in a readable format for the user
+    ''')
 
-    #print_results(top_3_articals,top_autohors);
-    print_results(,"test","test");
+    important_errors=cur.fetchall()
+
+    print_results(top_3_articals,top_autohors,important_errors);
+
 
 
 if __name__ == "__main__":
